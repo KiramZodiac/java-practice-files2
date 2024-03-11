c30076294f72648d502434a21e4b286a5e41ef69
Kiram zodiac <kiramzodiac@gmail.com>
2024-03-11 17:55:21 +0300

    Update Main.java
    removed a curly brackets that was causing an error in the application

--- Main.java
+++ Main.java
@@ -16,7 +16,7 @@ public class Main {
         public static void main(String[] args) {
 
              
-                }
+                
 
                 Scanner myScanner = new Scanner(System.in);
                 String digits;
